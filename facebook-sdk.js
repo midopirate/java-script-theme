abc623f60da53d1a55da28a65423f5f9fce02342
midopirate <dj.dhriwa2013@gmail.com>
2018-08-12 08:30:31 -0700

    Create facebook-sdk.js

---
+++ facebook-sdk.js
@@ -0,0 +1,12 @@
+<script>
+//<![CDATA[
+// Facebook SDK
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ar_AR/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+//]]>
+</script>
